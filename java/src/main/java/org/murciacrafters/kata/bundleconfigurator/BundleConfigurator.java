7d134bd6d7b2fadcc482b3f28b502210da13f12c
tmorenobernal <tmorenobernal@gmail.com>
2022-07-03 00:11:35 +0200

    Code to pass test

--- java/src/main/java/org/murciacrafters/kata/bundleconfigurator/BundleConfigurator.java
+++ java/src/main/java/org/murciacrafters/kata/bundleconfigurator/BundleConfigurator.java
@@ -21,7 +21,20 @@ public final class BundleConfigurator {
                 case P1 -> {
                     result.add("P1");
 
-                    if (result.contains("P2")) {
+                    if (result.contains("P2") && result.contains("P3") && result.contains("P4")) {
+                        result.remove("P1");
+                        result.remove("P2");
+                        result.remove("P3");
+                        result.remove("P4");
+                        result.add("B4");
+                    }
+                    else if (result.contains("B3") && result.contains("P2")) {
+                        result.remove("P1");
+                        result.remove("P2");
+                        result.remove("B3");
+                        result.add("B4");
+                    }
+                    else if (result.contains("P2")) {
                         result.remove("P1");
                         result.remove("P2");
                         result.add("B1");
@@ -31,12 +44,30 @@ public final class BundleConfigurator {
                         result.remove("P4");
                         result.add("B2");
                     }
+                    else if (result.contains("P5")) {
+                        result.remove("P1");
+                        result.remove("P5");
+                        result.add("B5");
+                    }
                 }
 
                 case P2 -> {
                     result.add("P2");
 
-                    if (result.contains("P1")) {
+                    if (result.contains("P1") && result.contains("P3") && result.contains("P4")) {
+                        result.remove("P1");
+                        result.remove("P2");
+                        result.remove("P3");
+                        result.remove("P4");
+                        result.add("B4");
+                    }
+                    else if (result.contains("B3") && result.contains("P1")) {
+                        result.remove("P1");
+                        result.remove("P2");
+                        result.remove("B3");
+                        result.add("B4");
+                    }
+                    else if (result.contains("P1")) {
                         result.remove("P1");
                         result.remove("P2");
                         result.add("B1");
@@ -45,20 +76,62 @@ public final class BundleConfigurator {
 
                 case P3 -> {
                     result.add("P3");
+
+                    if (result.contains("P1") && result.contains("P2") && result.contains("P4")) {
+                        result.remove("P1");
+                        result.remove("P2");
+                        result.remove("P3");
+                        result.remove("P4");
+                        result.add("B4");
+                    }
+                    else if (result.contains("B1") && result.contains("P4")) {
+                        result.remove("P3");
+                        result.remove("P4");
+                        result.remove("B1");
+                        result.add("B4");
+                    }
+                    else if (result.contains("P4")) {
+                        result.remove("P3");
+                        result.remove("P4");
+                        result.add("B3");
+                    }
                 }
 
                 case P4 -> {
                     result.add("P4");
 
-                    if (result.contains("P1")) {
+                    if (result.contains("P1") && result.contains("P2") && result.contains("P3")) {
+                        result.remove("P1");
+                        result.remove("P2");
+                        result.remove("P3");
+                        result.remove("P4");
+                        result.add("B4");
+                    }
+                    else if (result.contains("B1") && result.contains("P3")) {
+                        result.remove("P3");
+                        result.remove("P4");
+                        result.remove("B1");
+                        result.add("B4");
+                    }
+                    else if (result.contains("P1")) {
                         result.remove("P1");
                         result.remove("P4");
                         result.add("B2");
+                    } else if (result.contains("P3")) {
+                        result.remove("P3");
+                        result.remove("P4");
+                        result.add("B3");
                     }
                 }
 
                 case P5 -> {
                     result.add("P5");
+
+                    if (result.contains("P1")) {
+                        result.remove("P1");
+                        result.remove("P5");
+                        result.add("B5");
+                    }
                 }
             }
         }

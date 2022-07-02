4c2a3c1594a9aed588118dc5efc50a7b5c876347
tmorenobernal <tmorenobernal@gmail.com>
2022-07-02 21:15:20 +0200

    Code to pass test

--- java/src/main/java/org/murciacrafters/kata/bundleconfigurator/BundleConfigurator.java
+++ java/src/main/java/org/murciacrafters/kata/bundleconfigurator/BundleConfigurator.java
@@ -5,6 +5,10 @@ import java.util.List;
 public final class BundleConfigurator {
 
     public String calculateBestBuy(List<Product> products) {
+        if (products.isEmpty()) {
+            return "";
+        }
+
         return products.get(0).name();
     }
 }

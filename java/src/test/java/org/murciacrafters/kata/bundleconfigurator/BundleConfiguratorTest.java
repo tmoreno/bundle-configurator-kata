1e5a62d7956628d7652b45b67c397b46ff52e829
tmorenobernal <tmorenobernal@gmail.com>
2022-07-02 21:14:37 +0200

    Added test when buy nothing

--- java/src/test/java/org/murciacrafters/kata/bundleconfigurator/BundleConfiguratorTest.java
+++ java/src/test/java/org/murciacrafters/kata/bundleconfigurator/BundleConfiguratorTest.java
@@ -9,6 +9,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class BundleConfiguratorTest {
 
     @Test
+    void should_empty_when_buy_nothing() {
+        BundleConfigurator bundleConfigurator = new BundleConfigurator();
+
+        List<Product> products = List.of();
+        String bestBuyConfig = bundleConfigurator.calculateBestBuy(products);
+
+        assertThat(bestBuyConfig).isEqualTo("");
+    }
+
+    @Test
     void should_P1_when_buy_P1() {
         BundleConfigurator bundleConfigurator = new BundleConfigurator();
 

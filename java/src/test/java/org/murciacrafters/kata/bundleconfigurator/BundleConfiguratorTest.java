7a056e006c20fb89979707e0b11bb5f03ae49dee
tmorenobernal <tmorenobernal@gmail.com>
2022-07-02 21:08:30 +0200

    Added test when buy only P1 product

---
+++ java/src/test/java/org/murciacrafters/kata/bundleconfigurator/BundleConfiguratorTest.java
@@ -0,0 +1,20 @@
+package org.murciacrafters.kata.bundleconfigurator;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BundleConfiguratorTest {
+
+    @Test
+    void should_P1_when_buy_P1() {
+        BundleConfigurator bundleConfigurator = new BundleConfigurator();
+
+        List<Product> products = List.of(Product.P1);
+        String bestBuyConfig = bundleConfigurator.calculateBestBuy(products);
+
+        assertThat(bestBuyConfig).isEqualTo("P1");
+    }
+}

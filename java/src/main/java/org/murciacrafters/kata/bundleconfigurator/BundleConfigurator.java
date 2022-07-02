7b11b5e00174378df334e9dc4abba7ae30c451a3
tmorenobernal <tmorenobernal@gmail.com>
2022-07-02 23:55:20 +0200

    Code to pass test

--- java/src/main/java/org/murciacrafters/kata/bundleconfigurator/BundleConfigurator.java
+++ java/src/main/java/org/murciacrafters/kata/bundleconfigurator/BundleConfigurator.java
@@ -1,5 +1,6 @@
 package org.murciacrafters.kata.bundleconfigurator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class BundleConfigurator {
@@ -13,6 +14,55 @@ public final class BundleConfigurator {
             return products.get(0).name();
         }
 
-        return "B1";
+        List<String> result = new ArrayList<>();
+
+        for (Product product : products) {
+            switch (product) {
+                case P1 -> {
+                    result.add("P1");
+
+                    if (result.contains("P2")) {
+                        result.remove("P1");
+                        result.remove("P2");
+                        result.add("B1");
+                    }
+                    else if (result.contains("P4")) {
+                        result.remove("P1");
+                        result.remove("P4");
+                        result.add("B2");
+                    }
+                }
+
+                case P2 -> {
+                    result.add("P2");
+
+                    if (result.contains("P1")) {
+                        result.remove("P1");
+                        result.remove("P2");
+                        result.add("B1");
+                    }
+                }
+
+                case P3 -> {
+                    result.add("P3");
+                }
+
+                case P4 -> {
+                    result.add("P4");
+
+                    if (result.contains("P1")) {
+                        result.remove("P1");
+                        result.remove("P4");
+                        result.add("B2");
+                    }
+                }
+
+                case P5 -> {
+                    result.add("P5");
+                }
+            }
+        }
+
+        return String.join(",", result);
     }
 }

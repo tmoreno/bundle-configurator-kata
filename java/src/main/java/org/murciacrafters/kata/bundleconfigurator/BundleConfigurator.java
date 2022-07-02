74fed77b8633aea173ad897310e3503ff6d76af2
tmorenobernal <tmorenobernal@gmail.com>
2022-07-03 00:35:11 +0200

    Refactor. Simplify code

--- java/src/main/java/org/murciacrafters/kata/bundleconfigurator/BundleConfigurator.java
+++ java/src/main/java/org/murciacrafters/kata/bundleconfigurator/BundleConfigurator.java
@@ -6,14 +6,6 @@ import java.util.List;
 public final class BundleConfigurator {
 
     public String calculateBestBuy(List<Product> products) {
-        if (products.isEmpty()) {
-            return "";
-        }
-
-        if (products.size() == 1) {
-            return products.get(0).name();
-        }
-
         List<String> result = new ArrayList<>();
 
         for (Product product : products) {
